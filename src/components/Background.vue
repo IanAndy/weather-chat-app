9dfdd9e90d488f7d4284dfe2c0c45a3473ade734
Ianandy <ianandy83@gmail.com>
2022-06-13 13:28:20 +0300

    reduce opacity on background

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-full md:h-screen p-4 md:p-0 bg-black bg-no-repeat bg-cover bg-[url('./assets/Background.png')] flex flex-col justify-center items-center">
-    <div class="w-[90%] sm:w-[70%] md:w-[600px] backdrop-blur-sm bg-white/30 py-4 px-5 rounded-[1.875rem] text-center">
+    <div class="w-[90%] sm:w-[70%] md:w-[600px] backdrop-blur-sm bg-white/20 py-4 px-5 rounded-[1.875rem] text-center">
       <Weather/>
       <WeatherStatus/>
     </div>
